077f7e50a016df472a68954a66b9c223dbb6b075
Kai Wang <kkaiwwana@gmail.com>
2026-07-15 09:37:43 +0800

    update sync logger to support wandb/swanlab backend.

--- src/model/utils.py
+++ src/model/utils.py
@@ -1,14 +1,23 @@
 # import wandb
 import swanlab
+import wandb
 from pytorch_lightning.utilities import rank_zero_only
 
 
 class WanbSyncLogger:
     """Synchronized wandb logger. 
     The raw version's step counting is async, because of pl calls """
-    def __init__(self, run, log_every_n_steps=None, step_suffix='_step', epoch_suffix='_epoch'):
+    def __init__(self, run, log_every_n_steps=None, step_suffix='_step', epoch_suffix='_epoch', log_backend='wandb'):
         self.run = run
         self.log_every_n_steps = log_every_n_steps
+
+        if log_backend == 'wandb':
+            self.log_backend = wandb
+        elif log_backend == 'swanlab':
+            self.log_backend = swanlab
+        else:
+            raise ValueError(f'Invalid log backend specified: {log_backend}')
+
         self.cached_data = dict()
         self.current_step = 0
         self.old_current_epoch = 0
@@ -32,8 +41,7 @@ class WanbSyncLogger:
     @rank_zero_only
     def log(self, data: dict, on_step=None, on_epoch=None, step=None):
         if not self.log_every_n_steps or (on_step is None and on_epoch is None):
-            # wandb.log(data, step=self.current_step)
-            swanlab.log(data, step=self.current_step)
+            self.log_backend.log(data, step=self.current_step)
             return
         
         # if k in d, append v， else create [v]
@@ -49,9 +57,7 @@ class WanbSyncLogger:
             
             # 2. log & 3. reset
             if self.current_step % self.log_every_n_steps == 0:
-                # wandb.log({k + self._step_suffix: _list_reducer(self.cached_data[k + self._step_suffix])
-                #             for k in data.keys()}, step=self.current_step)
-                swanlab.log({k + self._step_suffix: _list_reducer(self.cached_data[k + self._step_suffix])
+                self.log_backend.log({k + self._step_suffix: _list_reducer(self.cached_data[k + self._step_suffix])
                             for k in data.keys()}, step=self.current_step)
                 for k in data.keys():
                     self.cached_data[k + self._step_suffix] = []
@@ -60,8 +66,7 @@ class WanbSyncLogger:
             # on-epoch metrics
             if self.current_epoch != self.old_current_epoch:
                 epoch_keys = list(filter(lambda x: self._epoch_suffix in x, self.cached_data.keys()))
-                # wandb.log({k: _list_reducer(self.cached_data[k]) for k in epoch_keys}, step=self.current_step)
-                swanlab.log({k: _list_reducer(self.cached_data[k]) for k in epoch_keys}, step=self.current_step)
+                self.log_backend.log({k: _list_reducer(self.cached_data[k]) for k in epoch_keys}, step=self.current_step)
                 for k in epoch_keys:
                     self.cached_data[k] = []
             else:
